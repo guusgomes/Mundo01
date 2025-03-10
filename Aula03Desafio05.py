511a97bd7bc8a3f61d1d1c5b08ce7d4fa4658ed9
Gustavo Gomes <guusgomes@hotmail.com>
2025-03-10 18:17:26 -0300

    Mundo01

---
+++ Aula03Desafio05.py
@@ -0,0 +1,5 @@
+print('='*6,'DESAFIO 06','='*6)
+n = int(input('Digite um número inteiro: '))
+sucessor = n + 1
+antecessor = n - 1
+print(f'Você digitou o número {n}, seu sucessor é o {sucessor} e seu antecessor é o {antecessor}!')
---
+++ Aula03Desafio11.py
@@ -0,0 +1,6 @@
+print('='*6,'DESAFIO 11','='*6)
+largura = float(input('Digite a largura da parede em metros: '))
+altura = float(input('Digite a altura da parede em metros: '))
+area = largura * altura
+litros = area / 2
+print(f'A área total da sua parede é de {area:.2f}m, são necessários {litros:.2f}L de tinta para pintá-la!')
